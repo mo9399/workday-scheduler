c55043b8b895487b5658ef005c15ef42b05a4a15
your name <you@example.com>
2022-06-14 00:26:48 -0700

    fix error in html to get localstorage to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,15 +15,15 @@ $(document).ready(function() {
     });
 
     //Load saved data from localStorage
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour17 .description").val(localStorage.getItem("hour-17"));
 
     function hourTracking() {
         //current number of hours
